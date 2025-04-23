700287c57a7b921fe8e28b362f0041112725bfd6
Durgesh Pandey <41140945+durgeshpd@users.noreply.github.com>
2025-04-23 07:43:53 +0530

    first commit

---
+++ database.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb+srv://durgeskp:mongodbcool@cluster0.dktzov8.mongodb.net/";
+
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'MyAppDB';
+
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('users');
+
+    const updateResult = await collection.updateOne(
+        { email: "durgeskp@egmail.com" },       
+        { $set: { country: "India" } }       
+      );
+
+    const findResult = await collection.find({}).toArray();
+    console.log('Found documents =>', findResult);
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
